b7ad43806cd36222ac6ac2c64e2889d7dcd71edf
João Henrique <joao.martins@betha.com.br>
2021-07-12 11:59:30 -0300

    estilização dos cards/findSingleCidade/pesquisa

--- js/controllers/appController.js
+++ js/controllers/appController.js
@@ -1,11 +1,20 @@
 angular.module("appWeather").controller("appController", function($scope, weatherService, $q) {
 
+    $scope.showSinglecity = false;
     $scope.showCidades = true;
 
     var cidades = [
-        {id: 1, nome: "criciuma"},
-        {id: 2, nome: "dourados"},
-        {id: 3, nome: "corumba"}
+        "criciuma",
+        "dourados",
+        "campo grande",
+        "florianopolis",
+        "campinas",
+        "mogi das cruzes",
+        "lages",
+        "rio de janeiro",
+        "manaus",
+        "belo horizonte",
+        "sao paulo"
     ]
 
     var promisses = [];
@@ -13,10 +22,11 @@ angular.module("appWeather").controller("appController", function($scope, weathe
     $scope.cidades = [];
 
     for(let i = 0 ; i < cidades.length; i++){       
-        var promisse = weatherService.findWeatherCity(cidades[i].nome).then((response) => { 
-            console.log(response.data)   
+        var promisse = weatherService.findWeatherCity(cidades[i]).then((response) => {  
+            console.log(response.data) 
             var cidade = {
                 id: response.data.id,
+                icon:  "assets/icons/" + response.data.weather[0].icon + ".png",
                 nome : response.data.name,
                 temp: (response.data.main.temp - 273.15).toFixed(0),
                 temp_max : (response.data.main.temp_max - 273.15).toFixed(0),
@@ -32,47 +42,35 @@ angular.module("appWeather").controller("appController", function($scope, weathe
     $q.all(promisses).then(function(results){
         $scope.cidades = results;
         console.log(results)
-    })
-
-    $scope.mostrarPrevisaoSemana = (id) => {
-        const cityId = id;
-        console.log(cityId);
-
-        weatherService.findFurther(id).then((response) => {
+    }) 
+    
+    $scope.loadSingle = (nome) => {
+        weatherService.findWeatherCity(nome).then((response) => {
             $scope.cidade = {
-                diaSemana: "",
-                temp_0: (response.data.list[0].main.temp - 273.15).toFixed(0),
-                temp_1: (response.data.list[8].main.temp - 273.15).toFixed(0),
-                temp_2: (response.data.list[16].main.temp - 273.15).toFixed(0),
-                temp_3: (response.data.list[24].main.temp - 273.15).toFixed(0),
-                temp_4: (response.data.list[32].main.temp - 273.15).toFixed(0)
+                id: response.data.id,
+                icon:  "assets/icons/" + response.data.weather[0].icon + ".png",
+                nome : response.data.name,
+                temp: (response.data.main.temp - 273.15).toFixed(0),
+                temp_max : (response.data.main.temp_max - 273.15).toFixed(0),
+                temp_min : (response.data.main.temp_min - 273.15).toFixed(0),
+                temp_feels_like : (response.data.main.feels_like - 273.15).toFixed(0) 
             }
-
-            var weekday = 0;
-
-            for (let i=0; i < 33; i+7) {
-                var data = new Date(response.data.list[i].dt_txt);
-                weekDay  = data.getDay();
-                console.log(weekDay)
-            }
-
-            if(weekday == 1){
-                $scope.cidade.diaSemana = "Segunda-feira"
-            } else if(weekday == 2){
-                $scope.cidade.diaSemana = "Terça-feira"
-            } else if(weekday == 3){
-                $scope.cidade.diaSemana = "Quarta-feira"
-            } else if(weekday == 4){
-                $scope.cidade.diaSemana = "Quinta-feira"
-            } else if(weekday == 5){
-                $scope.cidade.diaSemana = "Sexta-feira"
-            } else if(weekday == 6){
-                $scope.cidade.diaSemana = "Sábado"
-            } else if(weekday == 7){
-                $scope.cidade.diaSemana = "Domingo"
-            }
-            console.log($scope.cidade)
+            console.log(response.data);
         })
+    }
+
+    $scope.buscarCidade = (cidadeNome) => {
+        $scope.showCidades = false;
+        $scope.showSinglecity = true;
+        var input = document.querySelector("#search-cidade");
+        var cidadeNome = input.value; 
+        $scope.loadSingle(cidadeNome);
+        input.value = "";
+    }
+
+    $scope.mostrarCidades = () => {
+        $scope.showSinglecity = false;
+        $scope.showCidades = true;
     }
 
 });
