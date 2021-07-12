28d93ccca927110fde791be3cbd816c05bacb575
unknown <jou.098olo@gmail.com>
2021-07-11 23:59:06 -0300

    initial commit

---
+++ js/services/weatherService.js
@@ -0,0 +1,13 @@
+angular.module("appWeather").service("weatherService", function($http, config) {
+    
+    const apiKey = "304ffa35b779267ea5282771e39679f7"
+    
+    this.findWeatherCity = (cityName) => {
+        return $http.get(config.apiUrl + "weather?q=" + cityName + "&appid=" + apiKey);
+    }
+
+    this.findFurther = (cityId) => {
+        return $http.get(config.apiUrl + "forecast?id=" + cityId + "&appid=" + apiKey);
+    }
+
+});
---
+++ js/value/config.js
@@ -0,0 +1,5 @@
+angular.module("appWeather").constant("config", {
+
+    apiUrl: "http://api.openweathermap.org/data/2.5/"
+    //https://sistema-manutencao-betha.herokuapp.com
+});
