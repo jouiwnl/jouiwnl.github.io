4c7913b129b9fe61636c8ba94fed8e692220cd2b
Joao Henrique Leonor Martins <joao.martins@betha.com.br>
2021-07-15 17:54:24 -0300

    inserindo previsão semanal

--- js/controllers/appController.js
+++ js/controllers/appController.js
@@ -21,61 +21,92 @@ angular.module("appWeather").controller("appController", function($scope, weathe
         "sao paulo"
     ]
 
+    var diaSemana = [
+        "Domingo",
+        "Segunda-feira",
+        "Terça-feira",
+        "Quarta-feira",
+        "Quinta-feira",
+        "Sexta-feira",
+        "Sábado"
+    ]
+
+
     //array de promisses para o callback posterior
     var promisses = [];
+    var promisseDiaSemana = [];
 
-    //Array de cidades da tela principal
+    //Array
     $scope.cidades = [];
-    
-    //monta o array de cidades da tela principal do app
-    for(let i = 0 ; i < cidades.length; i++){    
-        $scope.showCidades = false;
-        $scope.showLoading = true;
-           
-        var promisse = weatherService.findWeatherCity(cidades[i]).then((response) => {  
+    $scope.previsaoDiaSemana = [];
 
-            //monta o objeto de cidade da tela principal
-            var cidade = {
-                id: response.data.id,
-                icon:  "assets/icons/" + response.data.weather[0].icon + ".png",
-                backgrounds: "assets/backgrounds/" + response.data.weather[0].icon + ".jpg",
-                nome : response.data.name,
-                temp: (response.data.main.temp - 273.15).toFixed(0),
-                temp_max : (response.data.main.temp_max - 273.15).toFixed(0),
-                temp_min : (response.data.main.temp_min - 273.15).toFixed(0),
-                temp_feels_like : (response.data.main.feels_like - 273.15).toFixed(0)
-            }
-            return cidade;
+    this.buildMap = (long, lat) => {
+        var map = new ol.Map({
+            target: 'map',
+            layers: [
+            new ol.layer.Tile({
+                source: new ol.source.OSM()
+            })
+            ],
+            view: new ol.View({
+            center: ol.proj.fromLonLat([long, lat]),
+            zoom : 11.9
+            })
+        });
+
+        //Adiciona um pin na localização em específico
+        var layer = new ol.layer.Vector({
+            source: new ol.source.Vector({
+                features: [
+                    new ol.Feature({
+                        geometry: new ol.geom.Point(ol.proj.fromLonLat([long, lat]))
+                    })
+                ]
+            })
+        });
+        map.addLayer(layer);
+    }
+    
+    $scope.loadData = () => {
+        //monta o array de cidades da tela principal do app
+        for(let i = 0 ; i < cidades.length; i++){    
+            $scope.showCidades = false;
+            $scope.showLoading = true;
+            
+            var promisse = weatherService.findWeatherCity(cidades[i]).then((response) => { 
+
+                //monta o objeto de cidade da tela principal
+                var cidade = {
+                    id: response.data.id,
+                    icon:  "assets/icons/" + response.data.weather[0].icon + ".png",
+                    backgrounds: "assets/backgrounds/" + response.data.weather[0].icon + ".jpg",
+                    nome : response.data.name,
+                    temp: (response.data.main.temp - 273.15).toFixed(0),
+                    temp_max : (response.data.main.temp_max - 273.15).toFixed(0),
+                    temp_min : (response.data.main.temp_min - 273.15).toFixed(0),
+                    temp_feels_like : (response.data.main.feels_like - 273.15).toFixed(0)
+                }
+                return cidade;
+            })
+            //adiciona no array de promisses cada cidade encontrada
+            promisses.push(promisse);
+        } 
+
+
+        //faz todas as promisses referentes ao array
+        $q.all(promisses).then(function(results){
+            $scope.showLoading = true;
+            $scope.cidades = results;
+            $scope.showLoading = false;
+            $scope.showCidades = true;           
         })
-        //adiciona no array de promisses cada cidade encontrada
-        promisses.push(promisse);
-    } 
-
-
-    //faz todas as promisses referentes ao array
-    $q.all(promisses).then(function(results){
-        $scope.showLoading = true;
-        $scope.cidades = results;
-        $scope.showLoading = false;
-        $scope.showCidades = true;
-    }) 
-    
+    }
+       
     //carrega apenas a cidade selecionada
     $scope.loadSingle = (nome) => {
         weatherService.findWeatherCity(nome).then((response) => {
-            //montar mapa segundo API
-            var map = new ol.Map({
-                target: 'map',
-                layers: [
-                new ol.layer.Tile({
-                    source: new ol.source.OSM()
-                })
-                ],
-                view: new ol.View({
-                center: ol.proj.fromLonLat([response.data.coord.lon, response.data.coord.lat]),
-                zoom : 11.9
-                })
-            });
+
+            this.buildMap(response.data.coord.lon, response.data.coord.lat);
 
             //montar objeto da cidade
             $scope.cidade = {
@@ -90,10 +121,42 @@ angular.module("appWeather").controller("appController", function($scope, weathe
                 wind : (response.data.wind.speed * 3.6).toFixed(0),
                 humidity : response.data.main.humidity,
                 pressure : response.data.main.pressure,
-                mapa : map
             }
+
+          
+            for(let i = 1 ; i < diaSemana.length; i++){    
+                
+                var promisse = weatherService.findFurther(response.data.coord.lat, response.data.coord.lon).then((res) => {
+                    var dataFormatada = new Date((res.data.daily[i].dt)*1000).getDay();
+                    var dia = diaSemana[dataFormatada]; 
+
+                    var previsaoSemana = {
+                        humidity: res.data.daily[i].humidity,
+                        pressure: res.data.daily[i].pressure,
+                        background : "assets/backgrounds/" + res.data.daily[i].weather[0].icon + "large.jpg",
+                        temp_day : (res.data.daily[i].temp.day - 273.15).toFixed(0),
+                        temp_evening : (res.data.daily[i].temp.eve - 273.15).toFixed(0),
+                        temp_max : (res.data.daily[i].temp.max - 273.15).toFixed(0),
+                        temp_min : (res.data.daily[i].temp.min - 273.15).toFixed(0),
+                        temp_night : (res.data.daily[i].temp.night - 273.15).toFixed(0),
+                        icon : "assets/icons/" + res.data.daily[i].weather[0].icon + ".png",
+                        wind_speed : (res.data.daily[i].wind_speed * 3.6).toFixed(0),
+                        nome_dia: dia
+                    }
+
+                    return previsaoSemana;
+                })
+                //adiciona no array de promisses cada dia da semana
+                promisseDiaSemana.push(promisse);
+                
+            }
+
+            $q.all(promisseDiaSemana).then(function(results){
+                $scope.cidade.previsaoSemana = results;     
+                console.log($scope.cidade)
+            })
         }).catch((error) => {
-            alert("Cidade não encontrada")
+            alert(error)
             location.reload();
         })
     }
@@ -101,18 +164,7 @@ angular.module("appWeather").controller("appController", function($scope, weathe
     $scope.loadSingleByCoords = (lat, lon) => {
         weatherService.findByCoords(lat, lon).then((response) => {
             //montar mapa segundo API
-            var map = new ol.Map({
-                target: 'map',
-                layers: [
-                new ol.layer.Tile({
-                    source: new ol.source.OSM()
-                })
-                ],
-                view: new ol.View({
-                center: ol.proj.fromLonLat([response.data.coord.lon, response.data.coord.lat]),
-                zoom : 11.9
-                })
-            });
+            this.buildMap(response.data.coord.lon, response.data.coord.lat);
 
             //montar objeto da cidade
             $scope.cidade = {
@@ -127,8 +179,6 @@ angular.module("appWeather").controller("appController", function($scope, weathe
                 wind : (response.data.wind.speed * 3.6).toFixed(0),
                 humidity : response.data.main.humidity,
                 pressure : response.data.main.pressure,
-                mapa : map
-
             }
         }).catch((error) => {
             alert("Cidade não encontrada")
@@ -143,7 +193,7 @@ angular.module("appWeather").controller("appController", function($scope, weathe
         var cidadeNome = input.value; 
         $scope.loadSingle(cidadeNome);
         input.value = "";
-        $scope.showSinglecity = true;
+        $scope.showInfoCity = true;
     }
 
     //ver informações de uma cidade já cadastrada
@@ -177,4 +227,6 @@ angular.module("appWeather").controller("appController", function($scope, weathe
             alert("Não foi possível definir a localização")
         }
     }
+
+    $scope.loadData();
 });
--- js/services/weatherService.js
+++ js/services/weatherService.js
@@ -3,15 +3,17 @@ angular.module("appWeather").service("weatherService", function($http, config) {
     const apiKey = "304ffa35b779267ea5282771e39679f7"
     
     this.findWeatherCity = (cityName) => {
-        return $http.get(config.apiUrl + "weather?q=" + cityName + "&appid=" + apiKey);
+        return $http.get(config.apiUrl + "weather?q=" + cityName + "&appid=" + apiKey + "&lang=pt_br");
     }
 
-    this.findFurther = (cityId) => {
-        return $http.get(config.apiUrl + "forecast?id=" + cityId + "&appid=" + apiKey);
+    this.findFurther = (lat, lon) => {
+        return $http.get(config.apiUrl + "onecall?lat=" + lat + "&lon=" + lon + "&exclude=current,minutely,hourly,alerts&appid=" + apiKey);
     }
 
     this.findByCoords = (lat, lon) => {
         return $http.get(config.apiUrl + "weather?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey);
     }
+
+    this.find
 
 });
